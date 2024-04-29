abcd331558c5ccc8d7aa4f54062dadd4ee7eb015
jfar41 <68749301+jfar41@users.noreply.github.com>
2024-04-28 19:26:33 -0700

    Added project reference to CodelLogic

--- obj/Debug/net8.0/Codele.AssemblyInfo.cs
+++ obj/Debug/net8.0/Codele.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Codele")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246b1eb48762049ba49a4a0a6d5f6cbe49f6d6d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Codele")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Codele")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
